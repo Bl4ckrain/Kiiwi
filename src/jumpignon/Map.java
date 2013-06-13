907835eab74e3fae907d329110db1b099384ade0
Max <>
2013-06-14 01:06:52 +0200

    Erste Rectangles sind jetzt auch dabei.

--- src/jumpignon/Map.java
+++ src/jumpignon/Map.java
@@ -22,7 +22,17 @@ public class Map {
                 Floor oFloor = new Floor(100);
                 oFloor.setImage(iFloor);
                 
+                Image iRect = new Image("resources/rect_01.png");
+                Rectangle oRect = new Rectangle(300, 420-75, 75, 75, 0 );
+                oRect.setImage(iRect);
+                
+                Image iRect2 = new Image("resources/rect_01.png");
+                Rectangle oRect2 = new Rectangle(500, 420-75, 75, 75, 0 );
+                oRect2.setImage(iRect2);
+                
                 renderlist.addItem(oFloor);
+                renderlist.addItem(oRect);
+                renderlist.addItem(oRect2);
                 
             break;
                   
